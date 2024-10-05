84ac6af64bf7f49619c549c5dac80d1432f808b3
Barby Kelly <barbykelly1@gmail.com>
2024-10-05 21:13:11 +0000

    Added order summary to checkout success page

--- bag/views.py
+++ bag/views.py
@@ -5,12 +5,16 @@ from products.models import Product
 # Create your views here.
 
 def view_bag(request):
-    """A view that renders the bag contents page"""
+    """
+    A view that renders the bag contents page
+    """
 
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """Add a quantity of the specified product to the shopping bag"""
+    """
+    Add a quantity of the specified product to the shopping bag
+    """
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
@@ -78,6 +82,7 @@ def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
