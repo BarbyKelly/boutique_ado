2f7c20cd5ce71021c510e6915c18e3ce79a4d63e
Barby Kelly <barbykelly1@gmail.com>
2024-10-17 21:37:01 +0000

    056 deployment part 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-barbykelly-boutiqueado-nk840uhxaj6.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-barbykelly-boutiqueado-nk840uhxaj6.ws.codeinstitute-ide.net', 'localhost'],
+['https://git.heroku.com/bk-boutique-ado.git']
 
 
 # Application definition
@@ -122,12 +123,18 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
